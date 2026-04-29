2b6cb4728627d00b42ac2ee4c8a2b1dd366ff955
AARVEE-JADUGAR <ahir70078@gmail.com>
2026-04-29 23:24:04 +0530

    Update Tarun's score from 85 to 88

--- Student Scoreboard/src/App.jsx
+++ Student Scoreboard/src/App.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 
 function App() {
   const [students, setStudents] = useState([
-    { id: 1, name: "Tarun", score: 85 },
+    { id: 1, name: "Tarun", score: 88 },
     { id: 2, name: "Royal", score: 89 },
     { id: 3, name: "Ravi", score: 90 },
     { id: 4, name: "Karan", score: 78 },
